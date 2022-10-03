79b2f973f5f4d085321362d129f5ff66ad3e8853
DvncanG <dvncan30@gmail.com>
2022-10-03 20:04:14 +0200

    Proyect Finished

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 let info = document.querySelector('#info');
-let imgs = document.querySelectorAll('.main__img');
+const imgs = ["alexanderplatz.jpeg", "hermannplatz.jpeg", "jungfernheide.jpeg", "konstanzer.jpeg", "kurfurstenstrabe.jpeg", "paulsternstrasse.jpeg", "unterdenlinden.jpeg", "westhafen.jpeg"];
 let main = document.querySelector('main');
 const parada = (ev) => {
-    let posicion = Math.floor(Math.random() * imgs.length)
+    
     console.log(imgs);
-    console.log(posicion);
     let img = ev.target;
+    do{
+     posicion = Math.floor(Math.random() * imgs.length)
+        console.log(posicion);
+    }while(imgs[posicion] == img.getAttribute('src').slice(img.getAttribute('src').lastIndexOf('/')+1))
     info.textContent = img.getAttribute("alt");
-    img.setAttribute("src", imgs[posicion].getAttribute("src"));
+    img.setAttribute("src", "./imagenes/" + imgs[posicion])
+    info.textContent = imgs[posicion].slice(0, imgs[posicion].lastIndexOf('.'));
 };
 main.addEventListener('click', parada);
